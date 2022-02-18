692fc2238ac22bbf361c7c64d195fe291340bfdc
Dennis Dang <dangggdennis@gmail.com>
2022-02-18 16:38:15 -0500

    print and flush buffer after every message, explain how to run test

--- test/test_pubsub.ml
+++ test/test_pubsub.ml
@@ -1,3 +1,5 @@
+(* dune runtest --no-buffer *)
+
 open Pubsub
 module C = Domainslib.Chan
 
@@ -5,11 +7,12 @@ let subs : (string, string subscription) Hashtbl.t = Hashtbl.create 1000
 
 let () =
   let chan1 = subscribe subs "topic-1" in
+  let chan2 = subscribe subs "topic-2" in
 
   let rec receive ch =
-    print_endline "receive msg";
     let v = C.recv ch in
-    Format.printf "received val=%s\n" v;
+    Format.printf "received val=%s@." v;
+    (* print_endline v; *)
     receive ch
   in
 
@@ -19,33 +22,18 @@ let () =
   publish subs "topic-1" "hello4";
   publish subs "topic-1" "hello5";
   publish subs "topic-1" "hello6";
+
   publish subs "topic-2" "world1";
+  publish subs "topic-2" "world2";
+  publish subs "topic-2" "world3";
+  publish subs "topic-2" "world4";
+  publish subs "topic-2" "world5";
+  publish subs "topic-2" "world6";
 
-  let _d1 = Domain.spawn (fun _ -> receive chan1) in
+  let d1 = Domain.spawn (fun _ -> receive chan1) in
+  let d2 = Domain.spawn (fun _ -> receive chan2) in
 
-  (* The trigger button *)
-  (* Domain.join _d1; *)
-
-  (* 
-  Really interesting behavior:
-
-  First run `dune runtest` with the Domain.join. The process rightfully never shuts down, but no messages
-  are ever printed.
-
-  Now comment out the Domain.join line. Run `dune runtest` again and now you'll see something like:
-  ```
-  test_pubsub alias test/runtest
-  receive msg
-  pubsub job donereceive msg
-  receive msg
-  receive msg
-  receive msg
-  receive msg
-  receive msg
-  ```
-
-  And again, run `dune runtest` for the third time. This time there are no messages.
-   *)
-
+  Domain.join d1;
+  Domain.join d2;
 
   print_string "pubsub job done"
